32a21ce47d06ad26ce6c6a1673af5684a7951067
Tom Vaidyan <tvaidyan@gmail.com>
2022-09-11 11:27:52 -0500

    scaffold header and footer, add logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app>
+    <header class="app-header">
+      <figure class="header-image-container">
+        <router-link to="/home">
+          <img class="header-image" :src="require('@/assets/vueflix-logo.png')" />
+        </router-link>
+      </figure>
+    </header>
     <v-main>
       <router-view/>
     </v-main>
+    <footer class="app-footer">
+      <span class="footer-text">
+        &copy; Thomson Vaidyan {{ currentYear }}
+      </span>
+    </footer>
   </v-app>
 </template>
 
@@ -12,7 +24,39 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    currentYear: new Date().getFullYear()
   }),
 }
 </script>
+
+<style>
+  .app-footer{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    padding: 1em;
+  }
+
+  .header-image{
+    max-height: 60px;
+  }
+
+  .header-image-container
+  {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-self: flex-start;
+    max-width: 20em;
+    padding: 1em;
+  }
+
+  .app-header
+  {
+    background-color: #66A3BA;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
