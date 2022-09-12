ec82add4727a53d174cca02a77638c466674aca5
Tom Vaidyan <tvaidyan@gmail.com>
2022-09-11 20:47:47 -0500

    scaffold main navigation, login status banner

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,9 +6,34 @@
           <img class="header-image" :src="require('@/assets/vueflix-logo.png')" />
         </router-link>
       </figure>
+
+      <nav class="main-navigation">
+        <ul class="navigation-list">
+          <li class="list-link">
+            <router-link to="/home">Home</router-link>
+          </li>
+          <li class="list-link">
+            <router-link to="/home">Link 2</router-link>
+          </li>
+          <li class="list-link">
+            <router-link to="/home">Link 3</router-link>
+          </li>
+        </ul>
+      </nav>
+
+      <aside class="header-greeting">
+        <section class="greeting-content">
+          <figure v-if="isLoggedIn">
+            <div>I'm signed in</div>
+          </figure>
+          <figure v-else>
+            <div>I'm not logged in, show sign-in</div>
+          </figure>
+        </section>
+      </aside>
     </header>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
     <footer class="app-footer">
       <span class="footer-text">
@@ -29,34 +54,88 @@ export default {
 }
 </script>
 
-<style>
-  .app-footer{
+<style lang="scss">
+.app-footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  padding: 1em;
+}
+
+.header-image {
+  max-height: 60px;
+}
+
+.header-image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: flex-start;
+  max-width: 20em;
+  padding: 1em;
+}
+
+.app-header {
+  background-color: #66A3BA;
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 75em) {
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    margin: auto;
+  }
+}
+
+.main-navigation {
+  display: flex;
+  justify-content: center;
+
+  .navigation-list {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    .list-link {
+      align-items: center;
+      display: flex;
+      padding: 1em;
+
+      a {
+        color: #fff;
+
+        &.router-link-active {
+          font-weight: bold;
+          font-size: 1.1em;
+        }
+      }
+    }
+  }
+}
+
+.header-greeting {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  padding: 1em;
+
+  @media only screen and (min-width: 75em){
+    justify-content: flex-end;
+   }
+
+  .greeting-content {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    background-color: white;
-    padding: 1em;
-  }
+    width: 100%;
 
-  .header-image{
-    max-height: 60px;
+    @media only screen and (min-width: 75em){
+      align-items: flex-end;
+    }
   }
-
-  .header-image-container
-  {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-self: flex-start;
-    max-width: 20em;
-    padding: 1em;
-  }
-
-  .app-header
-  {
-    background-color: #66A3BA;
-    display: flex;
-    flex-direction: column;
-  }
+}
 </style>
